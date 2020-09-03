5f51d16ed9f3e66c310343a312044118a2b6acf9
Tuija Sonkkila <tts@users.noreply.github.com>
2020-09-03 17:37:10 +0300

    Update write.R

--- write.R
+++ write.R
@@ -1,21 +1,25 @@
 library(httr)
+library(polite)
 library(rvest)
 library(tidyverse)
 library(lubridate)
 
+
 source("list.R")
-source("aalto.R")
-source("hy.R")
-source("oulu.R")
-source("tampere.R")
-source("turku.R")
-source("jyu.R")
-source("lappi.R")
-source("abo.R")
-source("hanken.R")
-source("uef.R")
+
+source("aalto_polite.R")
+source("hy.R") # RSS feed
+source("oulu_polite.R")
+source("tampere_polite.R")
+source("turku_polite.R")
+source("jyu_polite.R")
+source("lappi_polite.R")
+source("abo_polite.R")
+source("hanken_polite.R")
+source("uef_polite.R")
 
 key <- Sys.getenv("airtableKey")
+year_now <- year(Sys.Date())
 
 # POST args
 make_body <- function(title, link, date, university, id){
@@ -38,10 +42,12 @@ make_body <- function(title, link, date, university, id){
   
 }
 
+
 # 1. Delete all old event records from the base (TO DO),
 # 2. Populate it with new ones generated next
 
 events <- list_event_pages()
+
 write_aalto_event_records()
 write_hy_event_records()
 write_oulu_event_records()
@@ -52,6 +58,3 @@ write_lappi_event_records()
 write_abo_event_records()
 write_hanken_event_records()
 write_uef_event_records()
-
-
-
