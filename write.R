cbee9c06ae5e65cd1d5625fc2db253b77017e422
Tuija Sonkkila <tts@users.noreply.github.com>
2021-05-07 21:25:40 +0300

    Update write.R

--- write.R
+++ write.R
@@ -3,6 +3,7 @@ library(polite)
 library(rvest)
 library(tidyverse)
 library(lubridate)
+library(RSelenium)
 
 source("functions.R")
 
@@ -28,19 +29,44 @@ year_now <- year(Sys.Date())
 # Fetch event page URL's
 events <- list_event_pages()
 
+# Tampere: date and time with RSelenium
+# https://docs.ropensci.org/RSelenium/articles/basics.html#java-binary
+
+remDr <- remoteDriver(
+  remoteServerAddr = "localhost",
+  port = 4445L,
+  browserName = "firefox"
+)
+remDr$open()
+
 # 1. Delete all old event records from the base (TO DO),
 #    either via GUI or programmatically
 # 2. Populate the base with new records generated below
 
+print("Starting Aalto")
 write_aalto_event_records()
+print("Starting Hy")
 write_hy_event_records()
+print("Starting Oulu")
 write_oulu_event_records()
+print("Starting Tampere")
 write_tampere_event_records()
+print("Starting Turku")
 write_turku_event_records()
+print("Starting Jyu")
 write_jyu_event_records()
+print("Starting Lappi")
 write_lappi_event_records()
+print("Starting Åbo")
 write_abo_event_records()
+print("Starting Hanken")
 write_hanken_event_records()
+print("Starting UEF")
 write_uef_event_records()
+print("Starting Taide")
 write_taide_event_records()
+print("Starting LUT")
 write_lut_event_records()
+
+remDr$close()
+# and ctrl+c in the command prompt
